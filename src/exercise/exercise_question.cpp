537d46a9af9002818d886f743e54ecb6f6bfeac3
Yuji Yamauchi <yuu@isc.chubu.ac.jp>
2019-07-08 15:17:50 +0900

    commit

---
+++ src/exercise/exercise_question.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+class RobotSim{
+    public:
+        float x_pos;
+        float y_pos;
+};
+
+int main(void){
+    /*
+    namespace関連の課題が思いつかなかったです。
+    問題設定
+    1.ロボットの初期位置(0,0)を代入するコンストラクタとデストラクタを作成しなさい。
+    2.メンバ変数x_pos、y_posにロボットの位置(引数)を代入するクラス関数Set(float x, float y)を作成しなさい
+    3.x_pos,y_posの値を出力するクラス関数Print()を作成しなさい
+    4.main文でRobotSimクラスを2つ生成しなさい
+    5.main文で1つ目のロボットの位置(1.0,2.0)に、2つ目のロボット(2.0, 3.0)に、Setを利用して代入し、Printで確認しなさい
+    6.main文で2つのx_posとy_posを利用して、2つのロボットの距離を求めなさい　平方根は#include<math>を呼び出し、sqrt()関数を使用すれば実現できる
+    */
+
+    //do nothing
+}
